400f8863e80f6f9c825029aba4cb6737d1cfc758
nerihub <neri_150487@hotmail.com>
2021-03-26 13:43:37 -0600

    1 file modified

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
 const Base = () => {
+  const [nombre,setNombre]=useState('Pedro');
+  // esta funcino se ejecuta sola
+  useEffect (
+    () => {
+      setTimeout( ()=> {
+        setNombre('Neri')
+        },2000
+      );
+    }
+  )
 
   return (
 
     <div>
       <h1>Pagina Base de /</h1>
+      {nombre}
     </div>
   );
 }
